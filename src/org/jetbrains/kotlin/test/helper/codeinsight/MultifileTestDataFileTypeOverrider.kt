5812266beba84a7b176a26d6fc5a253a81d770d5
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2026-05-04 14:36:39 +0300

    Remove check if file belongs to the project
    - it is a slow operation prohibited on EDT
    - it's kinda implicitly check in `file.isTestDataFile(project)` call anyway

--- src/org/jetbrains/kotlin/test/helper/codeinsight/MultifileTestDataFileTypeOverrider.kt
+++ src/org/jetbrains/kotlin/test/helper/codeinsight/MultifileTestDataFileTypeOverrider.kt
@@ -1,11 +1,9 @@
 package org.jetbrains.kotlin.test.helper.codeinsight
 
-import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.fileTypes.FileType
 import com.intellij.openapi.fileTypes.impl.FileTypeOverrider
 import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.project.ProjectManager
-import com.intellij.openapi.roots.ProjectFileIndex
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.util.runIf
 import org.jetbrains.kotlin.test.helper.TestDataWorkspaceConfiguration
@@ -17,10 +15,6 @@ class MultifileTestDataFileTypeOverrider: FileTypeOverrider, DumbAware {
     override fun getOverriddenFileType(file: VirtualFile): FileType? {
         return runIf(file.isSupportedByExtension()) {
             val shouldInjectForAnyProject = ProjectManager.getInstance().openProjects.any { project ->
-                val isRequiredProject = ApplicationManager.getApplication().runReadAction<Boolean> {
-                    !ProjectFileIndex.getInstance(project).isInContent(file)
-                }
-                if (!isRequiredProject) return@any false
                 TestDataWorkspaceConfiguration.getInstance(project).injectMultifileTestDataFileType &&
                     file.isTestDataFile(project)
             }
