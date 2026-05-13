765b96ac55692718a61d793b851a11d6de7d7a0d
Kirill Rakhman <kirill.rakhman@jetbrains.com>
2026-05-13 13:05:59 +0300

    Suppress diagnostics in test data files

--- src/org/jetbrains/kotlin/test/helper/codeinsight/TestDataFileHighlightErrorFilter.kt
+++ src/org/jetbrains/kotlin/test/helper/codeinsight/TestDataFileHighlightErrorFilter.kt
@@ -1,5 +1,7 @@
 package org.jetbrains.kotlin.test.helper.codeinsight
 
+import com.intellij.codeInsight.daemon.impl.HighlightInfo
+import com.intellij.codeInsight.daemon.impl.HighlightInfoFilter
 import com.intellij.codeInsight.highlighting.HighlightErrorFilter
 import com.intellij.injected.editor.VirtualFileWindow
 import com.intellij.psi.PsiErrorElement
@@ -9,15 +11,7 @@ import org.jetbrains.kotlin.test.helper.isTestDataFile
 
 class TestDataFileHighlightErrorFilter : HighlightErrorFilter() {
     override fun shouldHighlightErrorElement(element: PsiErrorElement): Boolean {
-        return !(isInjectionInTestFile(element.containingFile, element) && element.isLanguageIgnored())
-    }
-
-    private fun isInjectionInTestFile(
-        containingFile: PsiFile,
-        element: PsiErrorElement
-    ): Boolean {
-        val virtualFile = containingFile.virtualFile
-        return virtualFile is VirtualFileWindow && virtualFile.delegate.isTestDataFile(element.project)
+        return !(element.containingFile.isInjectionInTestFile() && element.isLanguageIgnored())
     }
 
     private fun PsiErrorElement.isLanguageIgnored(): Boolean {
@@ -26,4 +20,15 @@ class TestDataFileHighlightErrorFilter : HighlightErrorFilter() {
             it.equals(languageId, ignoreCase = true)
         }
     }
+}
+
+class TestDataFileHighlightInfoFilter : HighlightInfoFilter {
+    override fun accept(highlightInfo: HighlightInfo, psiFile: PsiFile?): Boolean {
+        return psiFile == null || !psiFile.isInjectionInTestFile()
+    }
+}
+
+private fun PsiFile.isInjectionInTestFile(): Boolean {
+    val virtualFile = this.virtualFile
+    return virtualFile is VirtualFileWindow && virtualFile.delegate.isTestDataFile(project)
 }
